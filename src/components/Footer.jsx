eec5721686784d59d8e659fafdf3a5a052d4614a
sars723 <sarasalomonn@gmail.com>
2021-06-04 14:34:12 +0200

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import "../components/css/Footer.css";
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 class Footer extends Component {
   render() {
@@ -8,7 +8,7 @@ class Footer extends Component {
       <Container fluid className="Footer ">
         <Container>
           <Row>
-            <Col className="col-12 col-md-6 col-lg-3 mt-5 icons">
+            <Col className="col-12 col-md-6 col-lg-3 mt-4 icons">
               <a href="https://www.facebook.com/netflix">
                 <i className="fab fa-facebook-square"></i>
               </a>
@@ -27,39 +27,28 @@ class Footer extends Component {
             <Col className="col-12 col-md-6 col-lg-3">
               <ul>
                 <li>
+                  <a href="#">Audio and Subtitles</a>
+                </li>
+                <li>
                   <a href="#">Media Centrer</a>
                 </li>
                 <li>
                   <a href="#">Privacy</a>
                 </li>
                 <li>
-                  <a href="#">Audio and Subtitles</a>
-                </li>
-                <li>
-                  <a href="#">Investor Relations</a>
+                  <a href="#">Contact Us</a>
                 </li>
               </ul>
+              <Button variant="black">Service Code</Button>
+              <p>Ⓒ 1997-2019 Netflix, Inc.</p>
             </Col>
             <Col className="col-12 col-md-6 col-lg-3">
               <ul>
                 <li>
-                  <a href="#">Help Centre</a>
+                  <a href="#">Audio Description</a>
                 </li>
                 <li>
-                  <a href="#">Jobs</a>
-                </li>
-                <li>
-                  <a href="#">Terms of Use</a>
-                </li>
-              </ul>
-            </Col>
-            <Col className="col-12 col-md-6 col-lg-3">
-              <ul>
-                <li>
-                  <a href="#">Buy gift cards</a>
-                </li>
-                <li>
-                  <a href="#">Netflix Originals</a>
+                  <a href="#">Investor Relaions</a>
                 </li>
                 <li>
                   <a href="#">Legal Notices</a>
@@ -69,18 +58,32 @@ class Footer extends Component {
             <Col className="col-12 col-md-6 col-lg-3">
               <ul>
                 <li>
+                  <a href="#">Help Center</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
                   <a href="#">Cookie Preferences</a>
                 </li>
+              </ul>
+            </Col>
+            <Col className="col-12 col-md-6 col-lg-3">
+              <ul>
                 <li>
-                  <a href="#">Account</a>
+                  <a href="#">Gift Cards</a>
                 </li>
                 <li>
-                  <a href="#">FAQ</a>
+                  <a href="#">Terms of Use</a>
+                </li>
+                <li>
+                  <a href="#">Corporate Information</a>
                 </li>
               </ul>
             </Col>
           </Row>
         </Container>
+        r
       </Container>
     );
   }

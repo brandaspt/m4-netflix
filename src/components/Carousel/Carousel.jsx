15befe209cdab123ba24ef5e1602953430600f3f
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-09 10:48:32 +0100

    Changed the carousel to button only

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,17 +3,19 @@ import CarouselItem from "./CarouselCard"
 import LoadingSpinner from "../LoadingSpinner"
 import scrollFunction from "../common/scrollerFunction"
 import SwipeScroll from "../common/scrollClass"
+import CarouselButton from "./CarouselButton"
 import { Row } from "react-bootstrap"
 import "./Carousel.css"
 
 import { getFilms } from "../common/dataFetch"
 
 class Carousel extends React.Component {
+  
   state = {
     movies: [],
     isLoading: true,
     hoveringCardComms: false,
-    carouselHovered: false
+    carouselHovered: true
   }
 
   scrolling = false
@@ -41,27 +43,33 @@ class Carousel extends React.Component {
     }
   }
 
+  scroll = (num) => {
+    console.log(num)
+    scrollFunction(num, this.carouselItemsRow)
+  }
+
   render() {
     return (
       <Row className="carouselRow px-5 pt-4">
         <Row>
           <h2>{this.props.carouselName}</h2>
         </Row>
-        <Row
-          className={this.state.carouselHovered ? "carouselItemsRow carouselPadMar" : "carouselItemsRow"}
-          onMouseEnter={() => {document.body.style.overflow = "hidden"; document.body.style.paddingRight = '17px'; this.setState({...this.state, carouselHovered: true})}}
-          onMouseLeave = {() => {document.body.style.overflow = "visible"; document.body.style.paddingRight = '0px'; this.setState({...this.state, carouselHovered: false, hoveringCardComms: false})}}
-          onWheel={e => scrollFunction(e, this.state.hoveringCardComms, this.state.carouselHovered, this.carouselItemsRow)}
-          ref={e => (this.carouselItemsRow = e)}
-        >
-          {this.state.isLoading ? (
-            <LoadingSpinner />
-          ) : (
-            this.state.movies.map((movie, i) => {
-              return <CarouselItem {...movie} commHover={this.hoveringOverCardComms} key={i} />
-            })
-          )}
-        </Row>
+        <div className="carouselWithBtns">
+          <CarouselButton side='Left' scroll={this.scroll} />
+          <Row
+            className={this.state.carouselHovered ? "carouselItemsRow carouselPadMar" : "carouselItemsRow"}
+            ref={e => (this.carouselItemsRow = e)}
+          >
+            {this.state.isLoading ? (
+              <LoadingSpinner />
+            ) : (
+              this.state.movies.map((movie, i) => {
+                return <CarouselItem {...movie} commHover={this.hoveringOverCardComms} key={i} />
+              })
+            )}
+          </Row>
+          <CarouselButton side='Right' scroll={this.scroll} />
+        </div>
       </Row>
     )
   }
